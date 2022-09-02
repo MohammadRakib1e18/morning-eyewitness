9c3e16ee421ed224f7fc1e5ee8ae6f5a87d11725
MohammadRakib1e18 <trustedrakib@gmail.com>
2022-09-03 02:14:25 +0600

    created search result

--- js/app.js
+++ js/app.js
@@ -7,15 +7,34 @@ let loadCategories = () =>{
 
 let displayCategories = (newCatetories) =>{
     let newsCategoriesContainer = document.getElementById('news-categories');
-    console.log(newsCategoriesContainer);
     newCatetories.forEach(news => {
-        // console.log(news.category_name);
         let span = document.createElement('span');
+
         span.className='news-category';
         span.innerText = news.category_name;
-        console.log(span);
+        span.setAttribute('category_id', news.category_id);
+        span.onclick = displayButtonClicked;
+
         newsCategoriesContainer.appendChild(span);
     })
+    
+}
+
+
+let displayButtonClicked = (event) =>{
+    id = event.target.getAttribute('category_id');
+    document.getElementById('selected-category').innerText=event.target.innerText;
+
+    // fetched category-based data
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySelectedCategoryNews(data.data));
+}
+
+let displaySelectedCategoryNews = (data) => {
+    let totalNews = data.length;
+    document.getElementById('result-counter').innerText="( " + totalNews + " )";
 }
 
 loadCategories();
